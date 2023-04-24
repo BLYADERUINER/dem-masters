7171676158438974ae8be7bc77360de0c8e7569c
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-04-25 00:31:18 +0400

    added/started dom

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <img src="" alt="" />
+          <h2>Dem-Master: Демонтаж зданий любой сложности</h2>
+        </div>
+        <nav>
+        </nav>
+      </header>
+      <main>
+        <div className="react-slick"></div>
+        <section className="description"></section>
+        <section className="examples"></section>
+        <section className="contact"></section>
+      </main>
+      <footer>
+        <p></p>
+        <div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
